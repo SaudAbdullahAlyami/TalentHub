97702349c5d90f30bccd8bc9ade0b896b779e879
AbdulrahmanAliH <slozx11@gmail.com>
2023-12-02 22:57:22 +0300

    db

--- app/pages/Sign-in-out/Welcome.js
+++ app/pages/Sign-in-out/Welcome.js
@@ -1,49 +1,45 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text,TouchableOpacity,Image } from "react-native"
 export const Welcome = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome!</Text>
-      <View style={styles.buttons}>
-        <Button style={styles.button}
-          title="Sign In"
-          onPress={() => navigation.navigate("Sign In")}
-        />
-        <Button style={styles.button}
-          title="Sign Up"
-          onPress={() => navigation.navigate("Sign Up")}
-        />
-      </View>
+    
+   <View className="flex-1" style={{backgroundColor: "#00B365"}}>
+        <View className="flex-1 flex justify-around my-5">
+            <Text 
+                className="text-white font-bold text-4xl text-center top-6">
+                Let's Get Started!
+            </Text>
+            <View className="flex-row justify-center">
+                <Image source={require("../../assets/Soccer-bro.png")}
+                    style={{width: 350, height: 350}} />
+            </View>
+            <View className="space-y-4 bottom-8">
+                <TouchableOpacity
+                    onPress={()=> navigation.navigate('Sign Up')}
+                    className="py-3 bg-yellow-400 mx-7 rounded-xl">
+                        <Text className="text-xl font-bold text-center text-gray-700">Sign Up</Text>
+                </TouchableOpacity>
+                <View className="flex-row justify-center">
+                    <Text className="text-white font-semibold">Already have an account?</Text>
+                    <TouchableOpacity onPress={()=> navigation.navigate('Sign In')}>
+                        <Text className="font-semibold text-yellow-400"> Log In</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
     </View>
+
+
+    
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "900",
-    color:"#788eec",
-    marginBottom: 20,
-  },
-  buttons:{
-    flexGrow:0.05,
-    flexDirection:"column",
-    justifyContent:"space-between",
-    marginTop:50,
-  },
-  button: {
-    backgroundColor: "#788eec",
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-    height: 48,
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
+
+
+
+
+
+
+
+
+
