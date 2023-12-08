4874e63c43444d7d398ca75d14b8ee6e8d661a60
Ab-hamdi <117590783+Ab-hamdi@users.noreply.github.com>
2023-12-08 20:58:59 +0300

    d

--- app/pages/Sign-in-out/Signup.js
+++ app/pages/Sign-in-out/Signup.js
@@ -12,7 +12,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../component/config/config";
 import { StatusBar } from "react-native";
 import { db } from "../../component/config/config";
-import {ArrowLeftIcon} from 'react-native-heroicons/solid'
+import { ArrowLeftIcon } from 'react-native-heroicons/solid'
 import {
   doc,
   setDoc,
@@ -22,16 +22,20 @@ export const SignUp = ({ navigation }) => {
   const [fullName, setFullName] = useState("");
   const [role, setRole] = useState("");//Player OR Scout or ..
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [clubName, setclubName] = useState("");
+  const [description, setDescription] = useState("");
+  const [city, setCity] = useState("");
+
   const [value, setValue] = React.useState({
     email: "",
     password: "",
     error: "",
   });
 
-// for coloring radiobutton
+  // for coloring radiobutton
 
 
-//assign the radiobutton
+  //assign the radiobutton
   const handleRadioButtonPress = (value) => {
 
     setRole(value)
@@ -50,175 +54,220 @@ export const SignUp = ({ navigation }) => {
       });
       return;
     }
-    if(value.password!==confirmPassword){
-        setValue({
-            ...value,
-            error: "passwords dont match",
-          });
-          return;
+    if (value.password !== confirmPassword) {
+      setValue({
+        ...value,
+        error: "passwords dont match",
+      });
+      return;
     }
-    
-    
-    setTimeout(() => {
-        createUserWithEmailAndPassword(auth, value.email, value.password)
-      .then((userCredential) => {
-        //ADD in db
-        setDoc(doc(db, "users",userCredential.user.uid), { 
-           uid: userCredential.user.uid,
-           email:value.email ,
-           fullName:fullName,
-           role: role
-           });
-        console.log("Added Successfully");
 
-        setValue({
+
+    setTimeout(() => {
+      createUserWithEmailAndPassword(auth, value.email, value.password)
+        .then((userCredential) => {
+          //ADD in db
+          setDoc(doc(db, "users", userCredential.user.uid), {
+            uid: userCredential.user.uid,
+            email: value.email,
+            fullName: fullName,
+            clubName: clubName,
+            description: description,
+            city: city,
+            role: role
+          });
+          console.log("Added Successfully");
+
+          setValue({
             ...value,
             error: null,
           });
           navigation.navigate('Sign In');
-      })
-      .catch((error) => {
-        setValue({
-          ...value,
-          error: error.message,
+        })
+        .catch((error) => {
+          setValue({
+            ...value,
+            error: error.message,
+          });
         });
-      });
-      }, 300);
-    
+    }, 300);
+
   };
 
 
 
   return (
 
-    <View className="flex-1 bg-white"  style={{backgroundColor: "#00B365"}}>
-      <View  className="flex ">
+    <View className="flex-1 bg-white" style={{ backgroundColor: "#00B365" }}>
+      <View className="flex ">
         <View className="flex-row justify-start">
-          <TouchableOpacity onPress={()=> navigation.goBack()} 
-          className="bg-yellow-400 top-5 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
+          <TouchableOpacity onPress={() => navigation.goBack()}
+            className="bg-yellow-400 top-5 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
             <ArrowLeftIcon size="20" color="black" />
           </TouchableOpacity>
         </View>
 
       </View>
-      <View 
-        style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}} 
+      <View
+        style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
         className="flex-1 bg-white top-8 px-8 pt-8">
-          <View className="form space-y-2">
-            <KeyboardAwareScrollView
-        keyboardShouldPersistTaps="always"
-      ></KeyboardAwareScrollView>
+        <View className="form space-y-2">
+          <KeyboardAwareScrollView
+            keyboardShouldPersistTaps="always"
+          ></KeyboardAwareScrollView>
 
-                 <Text className="text-gray-700 top-1  ml-4">Full Name</Text>
-            <TextInput 
-              className="p-4 bg-gray-100 top-1 text-gray-700  rounded-2xl "
-               placeholder="Full Name"
-          placeholderTextColor="#aaaaaa"
-          onChangeText={(text) => setFullName(text)}
-          value={fullName}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-            />
+          <Text className="text-gray-700 top-1  ml-4">Full Name</Text>
+          <TextInput
+            className="p-4 bg-gray-100 top-1 text-gray-700  rounded-2xl "
+            placeholder="Full Name"
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(text) => setFullName(text)}
+            value={fullName}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
+          />
 
-            <Text className="text-gray-700 top-1 ml-4">Email Address</Text>
-            <TextInput 
-              className="p-4 bg-gray-100 top-1 text-gray-700 rounded-2xl"
-              placeholder="E-mail"
-          placeholderTextColor="#aaaaaa"
-          onChangeText={(text) => setValue({ ...value, email: text })}
-          value={value.email}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-            />
-
-
-             <Text className="text-gray-700 top-1 ml-4">Password</Text>
-            <TextInput 
+          <Text className="text-gray-700 top-1 ml-4">Email Address</Text>
+          <TextInput
             className="p-4 bg-gray-100 top-1 text-gray-700 rounded-2xl"
-              placeholderTextColor="#aaaaaa"
-          secureTextEntry
-          placeholder="Password"
-          onChangeText={(text) => setValue({ ...value, password: text })}
-          value={value.password}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
+            placeholder="E-mail"
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(text) => setValue({ ...value, email: text })}
+            value={value.email}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
+          />
+
+
+          <Text className="text-gray-700 top-1 ml-4">Password</Text>
+          <TextInput
+            className="p-4 bg-gray-100 top-1 text-gray-700 rounded-2xl"
+            placeholderTextColor="#aaaaaa"
+            secureTextEntry
+            placeholder="Password"
+            onChangeText={(text) => setValue({ ...value, password: text })}
+            value={value.password}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
           />
 
 
 
           <Text className="text-gray-700 top-2 ml-4">Confirm Password</Text>
-            <TextInput 
+          <TextInput
             className="p-4 bg-gray-100 top-2 text-gray-700 rounded-2xl"
-               placeholderTextColor="#aaaaaa"
-          secureTextEntry
-          placeholder="Confirm Password"
-          onChangeText={(text) => setConfirmPassword(text)}
-          value={confirmPassword}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-           />
-
-           
-          <View style={styles.inputRadio}>
-      <TouchableOpacity  className="text-gray-700 top-5 right-3 ml-4" onPress={() => handleRadioButtonPress('Player')}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <View
-            style={{
-              width: 20,
-              height: 20,
-              borderRadius: 10,
-              borderWidth: 2,
-              borderColor: 'black',
-              marginRight: 10,
-              backgroundColor:
-              role === 'Player' ? '#00b365' : 'transparent',
-               
-            }}
+            placeholderTextColor="#aaaaaa"
+            secureTextEntry
+            placeholder="Confirm Password"
+            onChangeText={(text) => setConfirmPassword(text)}
+            value={confirmPassword}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
           />
-          <Text>Player</Text>
-        </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity  className="text-gray-700  left-40 ml-4" onPress={() => handleRadioButtonPress('Coach')}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <View
-            style={{
-              width: 20,
-              height: 20,
-              borderRadius: 10,
-              borderWidth: 2,
-              borderColor: 'black',
-              marginRight: 10,
-              backgroundColor:
-              role === 'Coach' ? '#00b365' : 'transparent',
-            }}
-          />
-          <Text>Coach</Text>
-        </View>
-      </TouchableOpacity>
-
-
-    </View>
-
-             {value.error&&<Text  className="py-3 bottom-7" style={{color:"red",marginLeft:30,}}>{value.error}</Text>}
-
-
-            <TouchableOpacity   onPress={() => onRegisterPress()}
-              className="py-3 bg-yellow-400 bottom-7 rounded-xl">
-                <Text  className="text-xl  font-bold  text-center text-gray-700" > Create account
-                </Text>
-             </TouchableOpacity>
+          {/* ffffffffffffffffffffffffffffffffffffffffffffffff */}
+          {role === "Coach" && (
             
+            <TextInput
+              className="p-4 bg-gray-100 top-2 text-gray-700 rounded-2xl"
+              placeholderTextColor="#aaaaaa"
+              secureTextEntry
+              placeholder="Club Name"
+              onChangeText={(text) => setclubName(text)}
+              value={clubName}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+          )}
+
+          <Text className="text-gray-700 top-1 ml-4">Description</Text>
+          {role === "Coach" && (
+            <TextInput
+              className="p-4 bg-gray-100 top-2 text-gray-700 rounded-2xl"
+              placeholderTextColor="#aaaaaa"
+              secureTextEntry
+              placeholder="Description"
+              onChangeText={(text) => setDescription(text)}
+              value={description}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+          )}
+
+          <Text className="text-gray-700 top-1 ml-4">City </Text>
+          {role === "Coach" && (
+            <TextInput
+              className="p-4 bg-gray-100 top-2 text-gray-700 rounded-2xl"
+              placeholderTextColor="#aaaaaa"
+              secureTextEntry
+              placeholder="City"
+              onChangeText={(text) => setCity(text)}
+              value={city}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+          )}
+
+
+          <View style={styles.inputRadio}>
+            <TouchableOpacity className="text-gray-700 top-5 right-3 ml-4" onPress={() => handleRadioButtonPress('Player')}>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    borderRadius: 10,
+                    borderWidth: 2,
+                    borderColor: 'black',
+                    marginRight: 10,
+                    backgroundColor:
+                      role === 'Player' ? '#00b365' : 'transparent',
+
+                  }}
+                />
+                <Text>Player</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity className="text-gray-700  left-40 ml-4" onPress={() => handleRadioButtonPress('Coach')}>
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <View
+                  style={{
+                    width: 20,
+                    height: 20,
+                    borderRadius: 10,
+                    borderWidth: 2,
+                    borderColor: 'black',
+                    marginRight: 10,
+                    backgroundColor:
+                      role === 'Coach' ? '#00b365' : 'transparent',
+                  }}
+                />
+                <Text>Coach</Text>
+              </View>
+            </TouchableOpacity>
+
+
           </View>
-          <View className="flex-row justify-center bottom-7 mt-3">
-              <Text className="text-gray-500 font-semibold">
-                  Already have an account?
-              </Text>
-              <TouchableOpacity  onPress={()=> navigation.navigate('Sign In')}>
-                  <Text className="font-semibold text-yellow-500"> Log in</Text>
-              </TouchableOpacity>
-          </View>
-          
+
+          {value.error && <Text className="py-3 bottom-7" style={{ color: "red", marginLeft: 30, }}>{value.error}</Text>}
+
+
+          <TouchableOpacity onPress={() => onRegisterPress()}
+            className="py-3 bg-yellow-400 bottom-7 rounded-xl">
+            <Text className="text-xl  font-bold  text-center text-gray-700" > Create account
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+        <View className="flex-row justify-center bottom-7 mt-3">
+          <Text className="text-gray-500 font-semibold">
+            Already have an account?
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Sign In')}>
+            <Text className="font-semibold text-yellow-500"> Log in</Text>
+          </TouchableOpacity>
+        </View>
+
       </View>
     </View>
 
@@ -228,7 +277,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    marginTop:StatusBar.currentHeight,
+    marginTop: StatusBar.currentHeight,
   },
   title: {},
   logo: {
@@ -270,7 +319,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  
+
   buttonTitle: {
     color: "white",
     fontSize: 16,
@@ -291,7 +340,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   loading: {
-    zIndex:9,
+    zIndex: 9,
     position: "absolute",
     top: "50%",
     left: "50%",
