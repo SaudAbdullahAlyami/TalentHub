1bee57e268a174ab9f273a968d9791f08a803035
KeGoooo <saudalyami222@gmail.com>
2023-12-11 22:12:15 +0300

    aa

--- app/pages/player/PlayerNotification.js
+++ app/pages/player/PlayerNotification.js
@@ -10,66 +10,132 @@ import {
   TouchableOpacity,
   StatusBar,
   FlatList,
-  ScrollView,
 } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Avatar } from "react-native-paper";
-import { useAuthentication } from "../../useAuthentication";
 import {
   doc,
-  onSnapshot,
-  collection,
-  setDoc,
   getDocs,
+  collection,
   query,
-  where
+  updateDoc,
+  deleteDoc,onSnapshot,
+  where,arrayUnion
 } from "firebase/firestore";
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
 import { db, auth, firebaase } from "../../component/config/config";
 
 export const PlayerNotification = ({ navigation }) => {
-   const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  
   useEffect(() => {
     loadData();
-  }, [loadData]);
+  }, []);
 
   const loadData = async () => {
     try {
       const invitationsRef = collection(db, "invitations");
-      const q = query(invitationsRef, where("receiverUid", "==", auth.currentUser.uid));
+      const q = query(
+        invitationsRef,
+        where("receiverUid", "==", auth.currentUser.uid)
+      );
       const querySnapshot = await getDocs(q);
-  
+
       const invitations = [];
       querySnapshot.forEach((doc) => {
         invitations.push({ id: doc.id, ...doc.data() });
       });
-  
-      setData(invitations)
+
+      setData(invitations);
     } catch (error) {
       console.error("Error fetching invitations:", error);
     }
   };
 
 
+  const [members, setMembers] = useState([]); 
+  const [imageURL, setImageURL] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [age, setAge] = useState("");
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [position, setPosition] = useState(null);
+  const [level, setLevel] = useState(null);
+  
 
+
+
+  const deleteInvite = async (inviteId) => {
+    await deleteDoc(doc(db, "invitations", inviteId));
+    console.log("Deleted Successfully");
+  };
+
+  const handleInvite = async (text, inviteId, coachUid, playerUid) => {
+    try {
+      const invitationRef = doc(db, "invitations", inviteId);
+
+      if (text === "Accepted") {
+        // Update the invitation status to "Accepted"
+        await updateDoc(invitationRef, { status: text });
+
+        // Update the coach's document to add the player to the members array
+        const playerRef = doc(db, "users", playerUid);
+        const coachRef = doc(db, "users", coachUid);
+
+        const unsubscribe = onSnapshot(playerRef, (doc) => {
+          setFullName(doc.data().fullName);
+          setAge(doc.data().age);
+          setHeight(doc.data().height);
+          setWeight(doc.data().weight);
+          setLevel(doc.data().level);
+          setImageURL(doc.data().profileImage)
+          setPosition(doc.data().position)
+          setMembers(doc.data().members);
+      });
+
+        await updateDoc(coachRef, {
+          members: arrayUnion({ 
+            fullName: fullName, age: age, height:height, weight:weight,level:level,profileImage:imageURL,position:position
+           }),
+        });
+
+
+      } else {
+        // Update the invitation status to "Rejected"
+        await updateDoc(invitationRef, { status: text });
+
+        // Delete the invitation
+        await deleteInvite(inviteId);
+      }
+
+      // Refresh the data after handling the invitation
+      loadData();
+    } catch (error) {
+      console.error("Error handling invitation:", error);
+    }
+  };
 
   const render = ({ item }) => {
     return (
-      <View className="p-4 bg-gray-100 top-1 text-gray-700  rounded-2xl ">
-        
-        <Text>sender is:{item.senderUid}</Text>
-        <Button title="Accept" color={"green"}/>
-        <Button title="Rejact" color={"red"}/>
-        
+      <View style={{ padding: 16, backgroundColor: "#f0f0f0", borderRadius: 16, marginBottom: 16 }}>
+        <Avatar.Image size={100} source={{ uri: item.senderImage }} />
+        <Text>{item.massage}</Text>
+        <Button
+          title="Accept"
+          color={"green"}
+          onPress={() => handleInvite("Accepted", item.id, item.senderUid, item.receiverUid)}
+        />
+        <Button
+          title="Reject"
+          color={"red"}
+          onPress={() => handleInvite("Rejected", item.id, item.senderUid)}
+        />
       </View>
     );
   };
 
   return (
     <View className="flex-1 bg-white" style={{ backgroundColor: "#00B365" }}>
-      <View className="flex-row justify-start">
-        
-      </View>
+      <View className="flex-row justify-start"></View>
 
       <View
         style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
